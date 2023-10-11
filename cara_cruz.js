abdf93e440cb68a299441f2d7b3aa858ddeca88f
moha-02 <119495982+moha-02@users.noreply.github.com>
2023-10-11 17:35:30 +0200

    v1

---
+++ cara_cruz.js
@@ -0,0 +1,9 @@
+var p1 = 1;
+var p2 = 2;
+const coin = Math.floor(Math.random() * 2) + 1;
+
+if(coin == 1){
+    console.log('Ha salido 1 , has ganado!!!!')
+}else if(coin == 2){
+    console.log('Ha salido 2, has perdido!! ');
+}
