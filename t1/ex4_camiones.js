92f0bb8ab0d1c4726d358f77893eccd910638a9f
moha-02 <119495982+moha-02@users.noreply.github.com>
2023-10-28 22:50:40 +0200

    gitignore

--- t1/ex4_camiones.js
+++ t1/ex4_camiones.js
@@ -1,10 +1,11 @@
-var chofer = ["pep","juan","toemu"];
+var chofer = ["pep","juan","toemu","moha"];
 var camion = ["volvo","scania","man","iveco"];
 var nSemanas = 52;
 for(i = 0; i<nSemanas;i++){
-    for(j = 0;j<chofer.length;j++){
-        console.log("En la semana  "+i+" Conduce: "+chofer[j]+"-"+camion[j]);
+    for(j = 1;j<=chofer.length;j++){
+        console.log("En la semana  "+i+"  conduce: "+chofer[j]+" -"+camion[j]);
     }
     descanso = camion.pop();
     camion.unshift(descanso);    
 }
+  
